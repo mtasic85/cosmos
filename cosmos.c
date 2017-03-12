687a2c2c6a6e71546b81dd41f0451e045cf032a7
Marko Tasic <mtasic85@gmail.com>
2017-03-12 23:44:42 +0100

    tokenizer/parser

--- cosmos.c
+++ cosmos.c
@@ -21,7 +21,7 @@ struct co_tokenizer_t;
 struct co_ast_t;
 struct co_parser_t;
 
-struct co_token_t *co_token_new(enum co_symbol_t symbol, size_t begin, size_t end);
+struct co_token_t *co_token_new(enum co_symbol_t symbol, const char * str, size_t len, size_t begin, size_t end);
 bool co_token_free(struct co_token_t *token);
 
 struct co_tokenizer_t *co_tokenizer_new(char *path);
@@ -89,12 +89,16 @@ typedef enum co_symbol_t {
 
 typedef struct co_token_t {
     enum co_symbol_t symbol;
+    const char * str;
+    size_t len;
     size_t begin;
     size_t end;
 } co_token_t;
 
 typedef struct co_tokenizer_t {
     char *path;
+    char *source_code;
+    size_t source_code_len;
     struct co_token_t *token;
 } co_tokenizer_t;
 
@@ -106,7 +110,7 @@ typedef struct co_parser_t {
     struct co_tokenizer_t *tokenizer;
 } co_parser_t;
 
-struct co_token_t *co_token_new(enum co_symbol_t symbol, size_t begin, size_t end) {
+struct co_token_t *co_token_new(enum co_symbol_t symbol, const char * str, size_t len, size_t begin, size_t end) {
     struct co_token_t *token = NULL;
 
     if (!(token = malloc(sizeof(struct co_token_t)))) {
@@ -115,6 +119,8 @@ struct co_token_t *co_token_new(enum co_symbol_t symbol, size_t begin, size_t en
     }
 
     token->symbol = symbol;
+    token->str = str;
+    token->len = len;
     token->begin = begin;
     token->end = end;
     return token;
@@ -138,7 +144,26 @@ struct co_tokenizer_t *co_tokenizer_new(char *path) {
         return NULL;
     }
 
+    FILE *f = fopen(path, "r");
+
+    if (!f) {
+        co_c_error("IOError: Could not open file.");
+        free(tokenizer);
+        return NULL;
+    }
+
+    fseek(f, 0, SEEK_END);
+    size_t source_code_len = ftell(f);
+    char *source_code = malloc(source_code_len * sizeof(char));
+    fseek(f, 0, SEEK_SET);
+    size_t read_len = fread(source_code, sizeof(char), source_code_len, f);
+    fclose(f);
+
+    // FIXME: compare read_len and source_code_len
+
     tokenizer->path = path;
+    tokenizer->source_code = source_code;
+    tokenizer->source_code_len = source_code_len;
     return tokenizer;
 }
 
@@ -148,20 +173,40 @@ bool co_tokenizer_free(struct co_tokenizer_t *tokenizer) {
         return false;
     }
 
+    if (!tokenizer->source_code) {
+        co_c_error("MemoryError: Tokenizer's source code is null.");
+        return false;
+    }
+
+    free(tokenizer->source_code);
     free(tokenizer);
     return true;
 }
 
 void co_tokenizer_nextsym(struct co_tokenizer_t *tokenizer) {
-
+    
 }
 
 struct co_parser_t *co_parser_new(struct co_tokenizer_t *tokenizer) {
-    return NULL;
+    struct co_parser_t *parser = NULL;
+
+    if (!(parser = malloc(sizeof(struct co_parser_t)))) {
+        co_c_error("MemoryError: Could not allocate parser.");
+        return NULL;
+    }
+
+    parser->tokenizer = tokenizer;
+    return parser;
 }
 
 bool co_parser_free(struct co_parser_t *parser) {
-    return false;
+    if (!parser) {
+        co_c_error("MemoryError: Parser is null.");
+        return false;
+    }
+
+    free(parser);
+    return true;
 }
 
 bool co_parser_accept(struct co_parser_t *parser, enum co_symbol_t symbol) {
